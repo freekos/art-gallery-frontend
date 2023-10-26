c82436c90f34fff96dea9d1bebcc7405054401c7
freekos <gazizbolat2@gmail.com>
2023-10-26 23:20:12 +0600

    build: add pinia-persisted-store

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import "./assets/main.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
 
+const pinia = createPinia();
+pinia.use(piniaPersist);
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
