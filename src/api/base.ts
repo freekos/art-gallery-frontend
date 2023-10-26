c653584783f679e23da6ae6e09ea7bc57d7904d2
freekos <gazizbolat2@gmail.com>
2023-10-26 23:25:10 +0600

    fix: change access-key variable to env-variable

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,8 +1,6 @@
 import { createApi } from "unsplash-js";
 
-const ACCESS_KEY = "G-K_1l7KXOrAt72RfldpFCrq_BG_gmoIm_kAe2ZdNSc";
-
 export const unsplashApi = createApi({
-	accessKey: ACCESS_KEY,
+	accessKey: import.meta.env.VITE_ACCESS_KEY as string,
 	fetch,
 });
